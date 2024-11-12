82a4627e1bb4056940b76b3b3b2053111047c139
teedev78 <i7891011t@gmail.com>
2024-11-12 14:55:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-2xl text-green-500'>Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
